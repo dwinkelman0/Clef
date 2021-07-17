1c30671c3f903abdc817f705913e12c30846da76
Daniel Winkelman <dwinkelman3@gmail.com>
2021-07-17 14:26:41 -0400

    [utils] Queue implementation, unit tests, and CMakeLists

---
+++ src/util/PooledQueue.h
@@ -0,0 +1,141 @@
+// Copyright 2021 by Daniel Winkelman. All rights reserved.
+
+#pragma once
+
+#include <stdint.h>
+
+/**
+ * A queue implemented as a ring buffer. The underlying memory is statically
+ * allocated. It can contain a maximum of N - 1 elements. When firstIndex_ ==
+ * lastIndex_, the queue is considered to be empty, so it is not possible for
+ * all N elements to be simultaneously occupied.
+ */
+template <typename T, uint16_t N>
+class PooledQueue {
+ public:
+  class Iterator {
+    friend class PooledQueue;
+
+   private:
+    Iterator(const PooledQueue *queue, const uint16_t index)
+        : queue_(queue), index_(index) {
+      assert(index_ < N);
+      assertInBounds();
+    }
+
+   public:
+    inline explicit operator bool() const {
+      assertInBounds();
+      return queue_;
+    }
+    inline T *operator->() const {
+      assert(queue_);
+      assertInBounds();
+      return const_cast<T *>(queue_->data_) + index_;
+    }
+    inline T &operator*() const {
+      assert(queue_);
+      assertInBounds();
+      return const_cast<T *>(queue_->data_)[index_];
+    }
+    inline bool operator==(const Iterator &other) const {
+      assert(queue_);
+      assertInBounds();
+      return queue_ == other.queue_ && index_ == other.index_;
+    }
+
+    /**
+     * Get the next iterator in the queue.
+     */
+    inline Iterator next() const {
+      assertInBounds();
+      return Iterator(queue_, nextIndex(index_));
+    }
+
+   private:
+    void assertInBounds() const {
+      if (queue_) {
+        if (queue_->firstIndex_ < queue_->lastIndex_) {
+          assert((index_ >= queue_->firstIndex_) &&
+                 (index_ < queue_->lastIndex_));
+        } else {
+          assert(!((index_ < queue_->firstIndex_) &&
+                   (index_ >= queue_->lastIndex_)));
+        }
+      }
+    }
+
+   private:
+    const PooledQueue *queue_; /*!< Store a reference to the queue. nullptr
+                                  represents an invalid iterator. */
+    uint16_t index_;           /*!< Store the location within the queue. */
+  };
+
+ public:
+  PooledQueue() { static_assert(N > 1); }
+
+  inline Iterator first() const {
+    return Iterator(firstIndex_ == lastIndex_ ? nullptr : this, firstIndex_);
+  }
+  inline Iterator last() const {
+    return Iterator(firstIndex_ == lastIndex_ ? nullptr : this,
+                    prevIndex(lastIndex_));
+  }
+  inline uint16_t size() const {
+    int16_t diff = (int16_t)lastIndex_ - (int16_t)firstIndex_;
+    return diff < 0 ? diff + N : diff;
+  }
+
+  /**
+   * Allocate a spot in the queue and return the iterator. The iterator is
+   * invalid if there is no capacity left.
+   */
+  inline Iterator push() {
+    uint16_t nextLastIndex = nextIndex(lastIndex_);
+    if (nextLastIndex == firstIndex_) {
+      return Iterator(nullptr, 0);
+    }
+    uint16_t oldLastIndex = lastIndex_;
+    lastIndex_ = nextLastIndex;
+    return Iterator(this, oldLastIndex);
+  }
+
+  /**
+   * Allocate a spot in the queue and set the value. Returns whether the
+   * operation was successful.
+   */
+  inline bool push(const T &data) {
+    Iterator it = push();
+    if (it) {
+      *it = data;
+    }
+    return static_cast<bool>(it);
+  }
+
+  /**
+   * Free the first element in the queue. Returns whether the operation was
+   * successful. The memory of the old item is not reset.
+   */
+  inline bool pop() {
+    bool canPop = firstIndex_ != lastIndex_;
+    if (canPop) {
+      firstIndex_ = nextIndex(firstIndex_);
+    }
+    return canPop;
+  }
+
+ private:
+  inline static uint16_t nextIndex(const uint16_t index) {
+    return (index + 1) % N;
+  }
+  inline static uint16_t prevIndex(const uint16_t index) {
+    return (index + N - 1) % N;
+  }
+
+ private:
+  T data_[N]; /*!< Hold the data in a statically-allocated array. */
+  uint16_t firstIndex_ = 0; /*!< Index of the first item in the queue; indices
+                           are in the range [0, N) */
+  uint16_t lastIndex_ = 0; /*!< Index + 1 of the last item in the queue; indices
+                          are in the range [0, N) */
+};
---
+++ tests/util/PooledQueueTests.cc
@@ -0,0 +1,86 @@
+// Copyright 2021 by Daniel Winkelman. All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "util/PooledQueue.h"
+
+struct Dummy {
+  uint16_t x;
+  uint16_t y;
+  bool operator==(const Dummy &other) const {
+    return x == other.x && y == other.y;
+  }
+};
+
+TEST(PooledQueue, ZeroSize) {
+  PooledQueue<Dummy, 8> queue;
+  ASSERT_EQ(queue.size(), 0);
+  ASSERT_FALSE(queue.first());
+  ASSERT_FALSE(queue.last());
+  ASSERT_FALSE(queue.pop());
+}
+
+TEST(PooledQueue, Push) {
+  using Queue = PooledQueue<Dummy, 4>;
+  Queue queue;
+
+  Queue::Iterator it0 = queue.push();
+  ASSERT_TRUE(it0);
+  it0->x = 4;
+  it0->y = 8;
+  ASSERT_EQ(queue.size(), 1);
+  ASSERT_EQ(it0, queue.first());
+  ASSERT_EQ(*it0, *queue.first());
+  ASSERT_EQ(it0, queue.last());
+  ASSERT_EQ(*it0, *queue.last());
+
+  ASSERT_TRUE(queue.push({17, 37}));
+  ASSERT_EQ(queue.size(), 2);
+  Queue::Iterator it1 = queue.first().next();
+  ASSERT_EQ(it1, queue.last());
+  ASSERT_EQ(*it1, *queue.last());
+
+  ASSERT_TRUE(queue.push({42, 56}));
+  ASSERT_EQ(queue.size(), 3);
+  Queue::Iterator it2 = queue.first().next().next();
+  ASSERT_EQ(*it2, *queue.last());
+  ASSERT_FALSE(queue.push({0, 0}));
+
+  ASSERT_EQ(it0->x, 4);
+  ASSERT_EQ(it0->y, 8);
+  ASSERT_EQ(it1->x, 17);
+  ASSERT_EQ(it1->y, 37);
+  ASSERT_EQ(it2->x, 42);
+  ASSERT_EQ(it2->y, 56);
+}
+
+TEST(PooledQueue, Pop) {
+  using Queue = PooledQueue<Dummy, 4>;
+  Queue queue;
+  ASSERT_TRUE(queue.push({0, 0}));
+  ASSERT_EQ(queue.size(), 1);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_FALSE(queue.pop());
+  ASSERT_EQ(queue.size(), 0);
+
+  ASSERT_TRUE(queue.push({1, 1}));
+  ASSERT_TRUE(queue.push({2, 2}));
+  ASSERT_EQ(queue.size(), 2);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_EQ(queue.size(), 1);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_FALSE(queue.pop());
+  ASSERT_EQ(queue.size(), 0);
+
+  ASSERT_TRUE(queue.push({3, 3}));
+  ASSERT_TRUE(queue.push({4, 4}));
+  ASSERT_TRUE(queue.push({5, 5}));
+  ASSERT_EQ(queue.size(), 3);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_EQ(queue.size(), 2);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_EQ(queue.size(), 1);
+  ASSERT_TRUE(queue.pop());
+  ASSERT_FALSE(queue.pop());
+  ASSERT_EQ(queue.size(), 0);
+}
