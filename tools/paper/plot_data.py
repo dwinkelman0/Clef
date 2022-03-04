e3768c7e52296c19ee8b17d1b1fe9a237a082f9f
Daniel Winkelman <dwinkelman3@gmail.com>
2022-03-04 00:23:20 -0500

    [analysis] Trying multivariate regression

--- tools/paper/plot_data.py
+++ tools/paper/plot_data.py
@@ -22,13 +22,13 @@ def calculateShearThinningJointExp(t, xs, P, guess):
     PRange = max(P) - min(P)
     def eq(t, *params):
         #t, xs, P = independentVars
-        A, T, K12, K3, n = params
-        xsEst = A * (1 - np.exp(T * (t - minTime)))
-        vsEst = -A * T * np.exp(T * (t - minTime))
-        PEst = (K12 * (vsEst)) ** 1.2
+        A, B, T, K12, K3, n = params
+        xsEst = A * (1 - np.exp(T * (t - minTime)) + B * (t - minTime))
+        vsEst = A * (-T * np.exp(T * (t - minTime)) + B)
+        PEst = (K12 * (vsEst)) ** n
         PEst -= PEst[0]
-        return ((xsEst - xs) / xsRange)**2 + ((PEst - P) / PRange)**2
-    params = opt.curve_fit(eq, t, np.zeros(t.shape), guess)
+        return (((xsEst - xs) / xsRange)**2 + ((PEst - P) / PRange)**2)**0.5
+    params = opt.curve_fit(eq, t, np.zeros(t.shape), guess, bounds=((0, 0, float("-inf"), 0, 0, 1), (float("inf"), float("inf"), 0, float("inf"), float("inf"), 2)))
     return params
 
 def calculateShearThinningExp(t, data, guess):
@@ -74,41 +74,41 @@ if __name__ == "__main__":
         axs[0][0].set_ylabel("Displacement")
         axs[0][0].legend()
 
-        #try:
-        xsTail.plot(axs[0][1], "diff")
-        pressureTail.plot(axs[0][1], "pressure")
-        #xsParams = xsTail.performExponentialRegression((600, -1e2))
-        #xsParams, xsEq = calculateShearThinning(xsTail.data[:, 0], xsTail.data[:, 1], (200, 2e-2, -1e-1))
-        
-        #xsFit = xsTail.fromRegressionParameters(xsParams)
-        #xsFit.plot(axs[0][1], "diff_fit", "--")
-        #(xsTail - xsFit).plot(axs[0][1], "diff_residual", "--")
-        #print(xsParams)
-        #DataSeries(np.column_stack((xsTail.data[:, 0], xsEq(xsTail.data[:, 0], *xsParams[:3])))).plot(axs[0][1], "xs_fit", "--")
-        #pressureParams = pressureTail.performExponentialRegression((-100, -1e2))
-        #pressureParams, pressureEq = calculateShearThinning(pressureTail.data[:, 0], pressureTail.data[:, 1], (-100, 2e-2, -1e-1))
-        #print(pressureParams)
-        #DataSeries(np.column_stack((pressureTail.data[:, 0], pressureEq(pressureTail.data[:, 0], *pressureParams[:3])))).plot(axs[0][1], "pressure_fit", "--")
-        #pressureFit = pressureTail.fromRegressionParameters(pressureParams)
-        #pressureFit.plot(axs[0][1], "pressure_fit", "--")
-        #(pressureTail - pressureFit).plot(axs[0][1], "pressure_residual", "--")
-        A, T, K12, K3, n = calculateShearThinningJointExp(*interp(xsTail.data, pressureTail.data), (200, -1e-1, 1, 1, 1.2))[0]
-        print(A, T, K12, K3, n)
-        xsFit = A * (1 - np.exp(T * (xsTail.data[:, 0] - min(xsTail.data[:, 0]))))
-        vsFit = -A * T * np.exp(T * (xsTail.data[:, 0] - min(xsTail.data[:, 0])))
-        pressureFit = K12 * abs(vsFit)**n 
-        pressureFit -= pressureFit[0]
-        xsFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], xsFit)))
-        vsFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], vsFit)))
-        pressureFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], pressureFit)))
-        xsFitDS.plot(axs[0][1], "xsFit", "--")
-        vsFitDS.plot(axs[0][1], "vsFit", "--")
-        pressureFitDS.plot(axs[0][1], "pressureFit", "--")
-        axs[0][1].set_xlabel("Time (s)")
+        try:
+            xsTail.plot(axs[0][1], "diff")
+            pressureTail.plot(axs[0][1], "pressure")
+            #xsParams = xsTail.performExponentialRegression((600, -1e2))
+            #xsParams, xsEq = calculateShearThinning(xsTail.data[:, 0], xsTail.data[:, 1], (200, 2e-2, -1e-1))
+            
+            #xsFit = xsTail.fromRegressionParameters(xsParams)
+            #xsFit.plot(axs[0][1], "diff_fit", "--")
+            #(xsTail - xsFit).plot(axs[0][1], "diff_residual", "--")
+            #print(xsParams)
+            #DataSeries(np.column_stack((xsTail.data[:, 0], xsEq(xsTail.data[:, 0], *xsParams[:3])))).plot(axs[0][1], "xs_fit", "--")
+            #pressureParams = pressureTail.performExponentialRegression((-100, -1e2))
+            #pressureParams, pressureEq = calculateShearThinning(pressureTail.data[:, 0], pressureTail.data[:, 1], (-100, 2e-2, -1e-1))
+            #print(pressureParams)
+            #DataSeries(np.column_stack((pressureTail.data[:, 0], pressureEq(pressureTail.data[:, 0], *pressureParams[:3])))).plot(axs[0][1], "pressure_fit", "--")
+            #pressureFit = pressureTail.fromRegressionParameters(pressureParams)
+            #pressureFit.plot(axs[0][1], "pressure_fit", "--")
+            #(pressureTail - pressureFit).plot(axs[0][1], "pressure_residual", "--")
+            A, B, T, K12, K3, n = calculateShearThinningJointExp(*interp(xsTail.data, pressureTail.data), (200, 2e-2, -1e-1, 1, 1, 1.2))[0]
+            print(A, B, T, K12, K3, n)
+            t = xsTail.data[:, 0] - min(xsTail.data[:, 0])
+            xsFit = A * (1 - np.exp(T * t) + B * t)
+            vsFit = A * (-T * np.exp(T * t) + B)
+            pressureFit = K12 * abs(vsFit)**n 
+            pressureFit -= pressureFit[0]
+            xsFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], xsFit)))
+            vsFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], vsFit)))
+            pressureFitDS = DataSeries(np.column_stack((xsTail.data[:, 0], pressureFit)))
+            xsFitDS.plot(axs[0][1], "xsFit", "--")
+            vsFitDS.plot(axs[0][1], "vsFit", "--")
+            pressureFitDS.plot(axs[0][1], "pressureFit", "--")
+            axs[0][1].set_xlabel("Time (s)")
 
-        # n[key] = pressureParams[2] / xsParams[2]
-        # except Exception as exception:
-        #     print("EXCEPTION on \"{}\": {}".format(key, exception))
+        except Exception as exception:
+            print("EXCEPTION on \"{}\": {}".format(key, exception))
 
         massVsDisplacement.plot(axs[1][0], "mass vs. displacement")
 
